849b20e8a472d19daba20fef19ef14e151200070
Bhupendra Singh <itzbhupendra@gmail.com>
2022-07-21 22:09:15 +0530

    LeetCode challenge : lengthOfLongestSubstring Non Repeating values Only

---
+++ src/AlgoritmsLibrary/LeetCode/LongestSubString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.AlgoritmsLibrary
+{
+    internal class LongestSubString
+    {
+        public int lengthOfLongestSubstring(string s)
+        {
+            HashSet<char> seen = new HashSet<char> ();
+
+            int left = 0, right = 0 , max = 0;
+            while (right<s.Length)
+            {
+                char c = s[right];
+                if (seen.Contains(c))
+                {
+                    max = Math.Max(max, right - left + 1);
+                    seen.Add(c);
+                    right++;
+                }
+                else
+                {
+                    while (s[left]!= c)
+                    {
+                        seen.Remove(s[left]);
+                        left++;
+                    }
+                    s.Remove(c);
+                    left++;
+                }
+            }
+
+            return max;
+        }
+    }
+}
