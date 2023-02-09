d1bcb9865dc0f68590c1441ff228278f9d6f4c07
Bhupendra Singh <itzbhupendra@gmail.com>
2023-02-09 11:11:08 +0530

    Tournament Winner

---
+++ src/AlgoritmsLibrary/AlgoExpert/TournamentWinner.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmsLibrary.AlgoExpert
+{
+
+    /// <summary>
+    /// 
+    /*
+     * Competetion = [
+                    ["HTML", "C#"],
+                    ["C#", "Python"],
+                    ["Python", "HTML"],
+                    ]
+    Format--> [Away,. Home ]
+    0 means Home winner, 1 means Away
+
+
+    result  = [0, 0, 1]
+
+    Means: C# beats HTML
+           Python beats C#
+           Python Beats HTML
+    So Winner is Python
+    
+    */
+    /// </summary>
+    public  class TournamentWinnerClass
+    {
+
+        public static string TournamentWinner(List<List<string>> competitions, List<int> results)
+        {
+
+            Dictionary<string, int> dicFinal = new Dictionary<string, int>();
+            string winner = "";
+            int maxVal = 0;
+            for (int i = 0; i < competitions.Count(); i++)
+            {
+                if (!dicFinal.ContainsKey(competitions[i][0]))
+                    dicFinal.Add(competitions[i][0], 0);// Home
+                if(!dicFinal.ContainsKey(competitions[i][1]))
+                    dicFinal.Add(competitions[i][1], 0);// Away
+
+                if (results[i] == 1)
+                {
+                    dicFinal[competitions[i][0]]++;
+                    if (dicFinal[competitions[i][0]] > maxVal)
+                    {
+                        maxVal = dicFinal[competitions[i][0]];
+                        winner= competitions[i][0];
+                    }
+                   
+                }
+                else
+                {
+                    dicFinal[competitions[i][1]]++;
+                    if (dicFinal[competitions[i][1]] > maxVal)
+                    {
+                        maxVal = dicFinal[competitions[i][1]];
+                        winner = competitions[i][1];
+                    }
+                }
+
+                
+            }
+            // Write your code here.
+            return winner;
+        }
+    }
+}
--- src/UniteTestAlgorithms/AlgoTests.cs
+++ src/UniteTestAlgorithms/AlgoTests.cs
@@ -70,5 +70,28 @@ namespace UniteTestAlgorithms
             Assert.AreEqual(result, SortedSquaredArrayClass.SortedSquaredArray(array));
         }
 
+        [Test]
+        public void TournamentWinnerShouldPass2()
+        {
+            List<List<string>> competitions = new List<List<string>>();
+            List<string> competition1 = new List<string> {
+            "HTML", "C#"
+        };
+            List<string> competition2 = new List<string> {
+            "C#", "Python"
+        };
+            List<string> competition3 = new List<string> {
+            "Python", "HTML"
+        };
+            competitions.Add(competition1);
+            competitions.Add(competition2);
+            competitions.Add(competition3);
+            List<int> results = new List<int> {
+            0, 0, 1
+        };
+            string expected = "Python";
+            Assert.AreEqual(expected, TournamentWinnerClass.TournamentWinner(competitions, results));
+        }
+
     }
 }
